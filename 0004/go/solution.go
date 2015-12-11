886e57a0b7fceaf0a61fa288b327f1a37498daa7
Thomas David Fischer <tdf.tomfischer@gmail.com>
2015-12-11 11:31:34 -0600

    Added Go solution for problem 5. Formatted Go solution for problem 4.

--- 0004/go/solution.go
+++ 0004/go/solution.go
@@ -10,25 +10,25 @@ import "fmt"
 import "os"
 
 func palindromify(x int, y int, z int) int {
-     return (1e5 * z + 1e4 * y + 1e3 * x +
-     	    1e2 * x + 1e1 * y + z);
+	return (1e5*z + 1e4*y + 1e3*x +
+		1e2*x + 1e1*y + z)
 }
 
 /* Brute force :( */
 func main() {
-     for one := 9; one >= 1; one -= 1 {
-     	 for two := 9; two >= 0; two -= 1 {
-	     for three := 9; three >= 0; three -= 1 {
-	         var num = palindromify(three, two, one)
-	         for a := 100; a < 1000; a += 1 {
-	     	     for b := 100; b < 1000; b += 1 {
-		         if a * b == num {
-			     fmt.Println(num)
-			     os.Exit(0)
-			 }
-		     }
-	         }
-	     }
-	 }
-     }
+	for one := 9; one >= 1; one -= 1 {
+		for two := 9; two >= 0; two -= 1 {
+			for three := 9; three >= 0; three -= 1 {
+				var num = palindromify(three, two, one)
+				for a := 100; a < 1000; a += 1 {
+					for b := 100; b < 1000; b += 1 {
+						if a*b == num {
+							fmt.Println(num)
+							os.Exit(0)
+						}
+					}
+				}
+			}
+		}
+	}
 }
---
+++ 0005/go/solution.go
@@ -0,0 +1,11 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(2*3*5*2*3*7*2*11*13*2*17*19)
+}
